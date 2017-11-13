3d554ee763ec66eb0fa02266e1ee933451ca4a64
lilisheng <lilisheng@lilisheng-ThinkPad-X240>
2017-11-13 22:05:07 +0800

    增加老师和微信号之间的关系表

--- app/models/modules/mobile/WxUser.java
+++ app/models/modules/mobile/WxUser.java
@@ -137,6 +137,7 @@ public class WxUser extends GenericModel{
     			wxUser.isParent = true;
     			wxUser.isCommittee = XjlDwParentCommittee.isParentCommittee(wxUser.currentClass.classId, wxUser.wxOpenId);
     		}
+        	wxUser.isTeacher = XjlDwWxTeacher.isTeacher(wxUser.wxOpenId);
         	return wxUser;
         }
 	}
